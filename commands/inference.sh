7e412d93ade62207d1702e2ae6ea9cae263705fc
saagarparikh02 <saagardp@pyxer.ai>
2024-08-13 16:47:08 +0000

    inference command

--- commands/inference.sh
+++ commands/inference.sh
@@ -1,23 +1,9 @@
-# VITONHD_release_input_person_combine_garment_240epochs_paired
 CUDA_VISIBLE_DEVICES=0 python scripts/inference.py \
---config configs/inference/inference_VITONHD_paired.yaml \
+--config configs/inference/inference_custom_unpaired.yaml \
 --ckpt checkpoints/release/TPD_240epochs.ckpt \
---outdir inference_logs/VITONHD/VITONHD_release_input_person_combine_garment_240epochs_paired/ \
+--outdir inference_logs/custom/TPD_240epochs_unpaired/ \
 --seed 321 \
---batch_size 15 \
---predicted_mask_dilation 0 \
+--batch_size 32 \
 --C 5 \
 --H 512 \
---W 768 \
-
-
-# VITONHD_release_input_person_combine_garment_240epochs_unpaired
-CUDA_VISIBLE_DEVICES=1 python scripts/inference.py \
---config configs/inference/inference_VITONHD_unpaired.yaml \
---ckpt checkpoints/release/TPD_240epochs.ckpt \
---outdir inference_logs/VITONHD/VITONHD_release_input_person_combine_garment_240epochs_unpaired/ \
---seed 321 \
---batch_size 15 \
---C 5 \
---H 512 \
---W 768 \
+--W 768 
